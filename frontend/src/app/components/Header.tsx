023d58ebc0e69b55a045601647244b9716ba7482
tomyhas59 <yh9035926@naver.com>
2025-09-28 20:35:19 +0900

    handleLogout

--- frontend/src/app/components/Header.tsx
+++ frontend/src/app/components/Header.tsx
@@ -1,11 +1,18 @@
 "use client";
 
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 import { useUserStore } from "store/useUserStore";
 
 export default function Header() {
+  const router = useRouter();
   const user = useUserStore((state) => state.user);
   const logout = useUserStore((state) => state.logout);
+
+  const handleLogout = () => {
+    logout();
+    router.push("/");
+  };
   return (
     <header className="bg-white shadow-md px-4 sm:px-6 py-3 sm:py-4 flex flex-wrap justify-between items-center sticky top-0 z-50">
       <Link
@@ -25,7 +32,7 @@ export default function Header() {
               내 정보
             </Link>
             <button
-              onClick={logout}
+              onClick={handleLogout}
               className="text-gray-700 hover:text-blue-500 transition cursor-pointer"
             >
               로그아웃
